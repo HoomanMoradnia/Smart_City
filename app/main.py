8903423720dfb7b843f12a15af9738bc633e2b65
Hooman Moradnia <hoomanmoradnia@gmail.com>
2026-05-18 13:23:06 +0330

    Include routes in main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from app.routes.data_routes import router as data_router
 
+app = FastAPI(
+    title="Smart City Smart Grid API",
+    version="1.0.0"
+)
 
-app = FastAPI()
+app.include_router(data_router)
 
 @app.get('/')
 def home_page():
