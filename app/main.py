f2e2e6697388989bc8e714a96313c5ed9d5f6da0
Hooman Moradnia <hoomanmoradnia@gmail.com>
2026-05-19 10:16:11 +0330

    prediction router was added

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.routes.data_routes import router as data_router
 from app.routes.meter_routes import router as meter_router
 from app.routes.analytics_routes import router as analytics_router
+from app.routes.prediction_routes import router as prediction_router
 
 app = FastAPI(
     title="Smart City Smart Grid API",
@@ -11,6 +12,7 @@ app = FastAPI(
 app.include_router(data_router)
 app.include_router(meter_router)
 app.include_router(analytics_router)
+app.include_router(prediction_router)
 
 @app.get('/')
 def home_page():
