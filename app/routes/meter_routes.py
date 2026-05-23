327721a32b89e0fb7162448c51739989d8368348
Hooman Moradnia <hoomanmoradnia@gmail.com>
2026-05-23 12:24:39 +0330

    replace memory storage with db

--- app/routes/meter_routes.py
+++ app/routes/meter_routes.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
+from sqlalchemy.orm import Session
 from app.models.meter_model import MeterData
-from app.services.meter_storage import meter_data_storage
+from app.models.database_models import MeterRecord
+from app.database.db import SessionLocal
 
 
 router = APIRouter()
@@ -8,13 +10,39 @@ router = APIRouter()
 
 @router.post("/meter-data")
 def receive_meter_data(data: MeterData):
-    meter_data_storage.append(data.model_dump())
+    
+    db: Session = SessionLocal()
+
+    new_record = MeterRecord(
+        customer_id=data.customer_id,
+        region_id=data.region_id,
+        timestamp=data.timestamp,
+        consumption_kwh=data.consumption_kwh,
+        temperature=data.temperature
+    )
+
+    db.add(new_record)
+    db.commit()
+    db.flush(new_record)
+    db.close()
+
     return {
         "message": "Meter data received successfully.",
-        "data": data
     }
 
 
 @router.get("/latest-data")
 def get_latest_data():
-    return meter_data_storage[-10:]
+    
+    db: Session = SessionLocal()
+    
+    records = (
+        db.query(MeterRecord)
+        .order_by(MeterRecord.timestamp.desc())
+        .limit(10)
+        .all()
+    )
+
+    db.close()
+
+    return records
