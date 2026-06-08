3cf9da944f47786cc558b508e592df41a835b265
Hooman <hoomanmoradnia@gmail.com>
2026-06-08 22:45:15 +0330

    view route and static file path was added

--- app/main.py
+++ app/main.py
@@ -3,9 +3,11 @@ from app.routes.data_routes import router as data_router
 from app.routes.meter_routes import router as meter_router
 from app.routes.analytics_routes import router as analytics_router
 from app.routes.prediction_routes import router as prediction_router
+from app.routes.view_routes import router as view_router
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(
-    title="Smart City Smart Grid API",
+    title="Smart City",
     version="1.0.0"
 )
 
@@ -13,7 +15,15 @@ app.include_router(data_router)
 app.include_router(meter_router)
 app.include_router(analytics_router)
 app.include_router(prediction_router)
+app.include_router(view_router)
 
 @app.get('/')
 def home_page():
     return {"message": "Smart city API is running"}
+
+
+app.mount(
+    "/static",
+    StaticFiles(directory="app/static"),
+    name="static"
+)
