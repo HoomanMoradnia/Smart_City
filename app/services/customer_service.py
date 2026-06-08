f388eeb54bbcd7b4b852f243052ae53c37f43b6c
Hooman <hoomanmoradnia@gmail.com>
2026-06-08 22:50:10 +0330

    information about customers

---
+++ app/services/customer_service.py
@@ -0,0 +1,74 @@
+import pandas as pd
+
+DATA_PATH = "app/data/sanandaj_smart_grid_dataset_v2.csv"
+
+df = pd.read_csv(DATA_PATH)
+
+
+def get_customers():
+
+    customers = (
+        df.groupby("customer_id")
+        .agg({
+            "region_id": "first",
+            "customer_profile": "first",
+            "consumption_kwh": "mean"
+        })
+        .reset_index()
+    )
+
+    return customers.to_dict(orient="records")
+
+
+def get_customer_details(customer_id: int):
+
+    customer_df = df[
+        df["customer_id"] == customer_id
+    ]
+
+    if customer_df.empty:
+        return None
+
+    return {
+        "customer_id": customer_id,
+        "region_id":
+            customer_df["region_id"].iloc[0],
+
+        "profile":
+            customer_df["customer_profile"].iloc[0],
+
+        "avg_consumption":
+            round(
+                customer_df["consumption_kwh"].mean(),
+                2
+            ),
+
+        "max_consumption":
+            round(
+                customer_df["consumption_kwh"].max(),
+                2
+            ),
+
+        "records":
+            len(customer_df)
+    }
+
+
+def get_customer_chart_data(customer_id: int):
+
+    customer_df = df[
+        df["customer_id"] == customer_id
+    ].copy()
+
+    if customer_df.empty:
+        return None
+
+    chart_df = customer_df.tail(168)
+
+    return {
+        "labels":
+            chart_df["datetime"].tolist(),
+
+        "values":
+            chart_df["consumption_kwh"].tolist()
+    }
