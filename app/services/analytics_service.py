ed3b7ce3d668000ed0c83de26accc4139647be9b
Hooman <hoomanmoradnia@gmail.com>
2026-06-13 11:28:54 +0330

    some change in structure

--- app/services/analytics_service.py
+++ app/services/analytics_service.py
@@ -8,23 +8,14 @@ df = pd.read_csv(DATA_PATH)
 
 def average_consumption():
     avg = df["consumption_kwh"].mean()
-    return {
-        "average_consumption": round(avg, 3)
-    }
+    return {"average_consumption": round(avg, 3)}
 
 
 def peak_hours():
-    peak_data = (
-        df.groupby("hour")["consumption_kwh"]
-        .mean()
-        .sort_values(ascending=False)
-    )
+    peak_data = (df.groupby("hour")["consumption_kwh"].mean().sort_values(ascending=False))
     return peak_data.head(5).to_dict()
 
 
 def region_consumption():
-    region_data = (
-        df.groupby("region_id")["consumption_kwh"]
-        .mean()
-    )
+    region_data = (df.groupby("region_id")["consumption_kwh"].mean())
     return region_data.to_dict()
