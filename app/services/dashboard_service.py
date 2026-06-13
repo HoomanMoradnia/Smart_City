4b99ed1248272ce8ae543a9d3bb50ab4b2f2b8af
Hooman <hoomanmoradnia@gmail.com>
2026-06-13 11:25:20 +0330

    add some function for more information

--- app/services/dashboard_service.py
+++ app/services/dashboard_service.py
@@ -6,14 +6,42 @@ df = pd.read_csv(DATA_PATH)
 
 
 def dashboard_stats():
-
     return {
         "customers": int(df["customer_id"].nunique()),
         "regions": int(df["region_id"].nunique()),
-        "average_consumption": round(
-            df["consumption_kwh"].mean(), 2
-        ),
-        "max_consumption": round(
-            df["consumption_kwh"].max(), 2
-        )
+        "average_consumption": round(df["consumption_kwh"].mean(), 2),
+        "max_consumption": round(df["consumption_kwh"].max(), 2)
     }
+
+
+def region_chart_data():
+
+    region_data = (df.groupby("region_id")["consumption_kwh"].mean().reset_index())
+
+    return {
+        "labels": region_data["region_id"].tolist(),
+        "values": region_data["consumption_kwh"].round(2).tolist()
+    }
+
+
+def hourly_city_consumption():
+
+    hourly_data = (df.groupby("hour")["consumption_kwh"].mean().reset_index())
+
+    return {
+        "labels": hourly_data["hour"].tolist(),
+        "values": hourly_data["consumption_kwh"].round(2).tolist()
+    }
+
+
+def top_consumers():
+    consumers = (df.groupby("customer_id")["consumption_kwh"].sum().sort_values(ascending=False).head(5))
+
+    return consumers.to_dict()
+
+
+def alert_statistics():
+
+    alerts = (df["alert"].value_counts().to_dict())
+
+    return alerts
